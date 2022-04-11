ebe7865bdbc2715ada50ccf12eb345d84117e3ed
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-04-11 18:24:24 +0200

    Add listAllCustomers(boolean), listSpecificBranchCustomers(String, boolean) methods to Bank class

--- src/com/arrayLists/banking/Bank.java
+++ src/com/arrayLists/banking/Bank.java
@@ -44,6 +44,63 @@ public class Bank {
         }
     }
 
+    public void listAllCustomers(boolean withTransactions) {
+        if(branches.size() == 0) {
+            System.out.println("There are no branches nor customers added yet");
+            return;
+        }
+
+        Branch currentlyPrintedBranch;
+        Customer currentlyPrintedCustomer;
+        System.out.println("Bank name: " + name);
+
+        for(int i = 0; i < branches.size(); i++) {
+            currentlyPrintedBranch = branches.get(i);
+            System.out.println("\tBranch " + currentlyPrintedBranch.getName() + ":");
+
+            for(int j = 0; j < currentlyPrintedBranch.getCustomers().size(); j++) {
+                currentlyPrintedCustomer = currentlyPrintedBranch.getCustomers().get(j);
+                System.out.println("\t\t" + currentlyPrintedCustomer.getName());
+
+                if(withTransactions) {
+                    ArrayList<Double> currentlyPrintedTransactions = currentlyPrintedCustomer.getTransactions();
+                    System.out.print("\t\t\t");
+
+                    for(int k = 0; k < currentlyPrintedTransactions.size(); k++) {
+                        System.out.print(currentlyPrintedTransactions.get(k) + "  ");
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    public void listSpecificBranchCustomers(String branchName, boolean withTransactions) {
+        System.out.println("Branch " + branchName);
+        Branch branchToPrint = findBranch(branchName);
+
+        if(branchToPrint == null) {
+            System.out.println("There is no branch " + branchName + " in this bank");
+            return;
+        }
+
+        Customer currentlyPrintedCustomer;  // created this variable for optimization purposes (fewer method calls this way)
+
+        for(int i = 0; i < branchToPrint.getCustomers().size(); i++) {
+            currentlyPrintedCustomer = branchToPrint.getCustomers().get(i);
+            System.out.println("\t" + currentlyPrintedCustomer.getName());
+
+            if(withTransactions) {
+                System.out.println("\t\t");
+
+                for(int j = 0; j < currentlyPrintedCustomer.getTransactions().size(); j++) {
+                    System.out.print(currentlyPrintedCustomer.getTransactions().get(j) + "  ");
+                }
+            }
+        }
+    }
+
     private Branch findBranch(String branchName) {
         for(int i = 0; i < branches.size(); i++) {
             if(branches.get(i).getName().equals(branchName)) {
